9f1955e522c55cc4310d134c264ab0e60c0d8858
rolandLKCSi <lukacsiroland01@gmail.com>
2017-05-01 21:38:29 +0200

    initial revision

---
+++ gameoflife.cpp
@@ -0,0 +1,12 @@
+/*
+* A Game of Life implementation from:
+* Roland Lukácsi
+*/
+#include<iostream>
+
+int main(){
+
+  std::cout << "This is the main file of the game of life project!!"<<std::endl;
+
+  return 0;
+}
