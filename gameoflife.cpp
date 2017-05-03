426c271822f34f9068e4ffd53511425cfc8424bb
rolandLKCSi <lukacsiroland01@gmail.com>
2017-05-03 17:25:22 +0200

    Function init added

--- gameoflife.cpp
+++ gameoflife.cpp
@@ -2,11 +2,29 @@
 * A Game of Life implementation from:
 * Roland Lukácsi
 */
+
 #include<iostream>
+
+int initialize();
 
 int main(){
 
-  std::cout << "This is the main file of the game of life project!!"<<std::endl;
+  int input;
+  
+  std::cout << "Game of Life!!\n\n\n";
+  std::cout << "Press any key...";
+  std::cin  >> input;
+
+  int init = 1;
+  if(init = initialize())
+    std::cout << "Successful init";
+  else
+    std::cout << "Something went wrong at init : "<<init<<std::endl;
 
   return 0;
+}
+
+int initialize(){
+  /*Init field for example*/
+  return 1;
 }
